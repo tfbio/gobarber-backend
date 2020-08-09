50729f7c5fa893ac1272f8fc1a1bcc4bf4ca739b
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-08-09 13:47:52 -0300

    Daily/monthly availability controller added

---
+++ src/modules/appointments/infra/http/controllers/ProviderMontlyAvailabilityController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderMonthlyAvailabilityService from '@modules/appointments/services/ListProviderMonthlyAvailabilityService';
+
+export default class ProviderMonthlyAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id, month, year } = request.body;
+
+    const listProviderMonthlyAvailabilityService = container.resolve(
+      ListProviderMonthlyAvailabilityService
+    );
+
+    const availability = await listProviderMonthlyAvailabilityService.execute({
+      provider_id,
+      month,
+      year,
+    });
+
+    return response.status(200).json(availability);
+  }
+}
