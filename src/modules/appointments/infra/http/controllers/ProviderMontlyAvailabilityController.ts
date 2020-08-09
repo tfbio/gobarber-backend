3c8058fe1b503c05ff0310563208707b86851d43
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-08-09 14:02:51 -0300

    Availability routes added

--- src/modules/appointments/infra/http/controllers/ProviderMontlyAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMontlyAvailabilityController.ts
@@ -5,7 +5,8 @@ import ListProviderMonthlyAvailabilityService from '@modules/appointments/servic
 
 export default class ProviderMonthlyAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { provider_id, month, year } = request.body;
+    const { provider_id } = request.params;
+    const { month, year } = request.body;
 
     const listProviderMonthlyAvailabilityService = container.resolve(
       ListProviderMonthlyAvailabilityService
--- src/modules/appointments/infra/http/routes/providers.route.ts
+++ src/modules/appointments/infra/http/routes/providers.route.ts
@@ -2,12 +2,25 @@ import { Router } from 'express';
 
 import ensureAuthenticate from '@modules/users/infra/http/middlewares/ensureAuthenticate';
 import ProviderController from '../controllers/ProviderController';
+import ProviderMontlyAvailabilityController from '../controllers/ProviderMontlyAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 
 const providerController = new ProviderController();
+const monthlyAvailabilityController = new ProviderMontlyAvailabilityController();
+const dailyAvailabilityController = new ProviderDayAvailabilityController();
+
 providersRouter.use(ensureAuthenticate);
 
 providersRouter.get('/', providerController.index);
+providersRouter.get(
+  '/:provider_id/monthly_availability',
+  monthlyAvailabilityController.index
+);
+providersRouter.get(
+  '/:provider_id/daily_availability',
+  dailyAvailabilityController.index
+);
 
 export default providersRouter;
