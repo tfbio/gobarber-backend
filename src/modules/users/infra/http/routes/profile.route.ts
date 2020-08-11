f3988793c72f289a34363ddbb8331789dd8325eb
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-08-11 14:29:53 -0300

    Profile routes joi validation

--- src/modules/users/infra/http/routes/profile.route.ts
+++ src/modules/users/infra/http/routes/profile.route.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticate from '@modules/users/infra/http/middlewares/ensureAuthenticate';
 
@@ -10,6 +11,18 @@ profileRouter.use(ensureAuthenticate);
 const profileController = new ProfileController();
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string(),
+      email: Joi.string().email(),
+      oldPassword: Joi.string(),
+      newPassword: Joi.string(),
+      password_confirm: Joi.string().valid(Joi.ref('newPassword')),
+    },
+  }),
+  profileController.update
+);
 
 export default profileRouter;
