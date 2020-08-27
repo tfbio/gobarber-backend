9b0da9a83d200c95aad4c1cd6b881df9c427d5e0
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-08-27 10:02:39 -0300

    rateLimiter added

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -13,8 +13,10 @@ import '@shared/infra/typeorm';
 import '@shared/container';
 
 import routes from '@shared/infra/http/routes';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.tempFolder));
